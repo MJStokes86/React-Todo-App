2bf4d2c7fd6c1e2c4f104d0093e0f1b136e56000
mjstokes86 <mjstokes1986@att.net>
2020-07-03 15:31:05 -0400

    initial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+import App from '../App'
+
+
+
+function TodoItem (props) {
+    return (
+    <div className = "todo-item">
+      <input type="checkbox" checked={props.item.completed}/>
+       <p>{props.item.text}</p>
+    </div>
+    )
+}
+
+export default TodoItem
