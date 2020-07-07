8076f701ee9344cda573d5f0f89c479c8315c4af
mjstokes86 <mjstokes1986@att.net>
2020-07-06 23:58:34 -0400

    added event handler for checkbox

--- src/App.js
+++ src/App.js
@@ -13,10 +13,25 @@ class App extends Component {
     this.state = {
       todos: todosData // imports data from todoData.js
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo // returns todo item
+      }) // map ends
+      return {
+        todos: updatedTodos
+      }
+    }) // setState ends
+  } // event handler ends
+
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item} />)
+    const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item} handleChange = {this.handleChange}/>)
     return (
       <div className="todo-list">
         {todoItems}
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,11 @@ import App from '../App'
 function TodoItem (props) {
     return (
     <div className = "todo-item">
-      <input type="checkbox" checked={props.item.completed}/>
+      <input type="checkbox" 
+      checked={props.item.completed} 
+      onChange = {() => props.handleChange(props.item.id)}
+      
+      />
        <p>{props.item.text}</p>
     </div>
     )
